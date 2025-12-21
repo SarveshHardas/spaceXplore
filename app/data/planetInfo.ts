36877a1064deaf10e51dfdc60bac47a1d1c01279
Sarvesh Hardas <hardassarvesh58@gmail.com>
2025-12-22 00:46:45 +0530

    Added planet list

---
+++ app/data/planetInfo.ts
@@ -0,0 +1,47 @@
+export const planets = [
+    {
+        name: "Mercury",
+        image: "/planets/mercury.png",
+        description: "Smallest planet and closest to the Sun.",
+    },
+    {
+        name: "Venus",
+        image: "/planets/venus.png",
+        description: "Hottest planet with a thick atmosphere.",
+    },
+    {
+        name: "Earth",
+        image: "/planets/earth.png",
+        description: "Only planet known to support life.",
+    },
+    {
+        name: "Mars",
+        image: "/planets/mars.png",
+        description: "The Red Planet, possible future home.",
+    },
+    {
+        name: "Jupiter",
+        image: "/planets/jupiter.png",
+        description: "Largest planet with powerful storms.",
+    },
+    {
+        name: "Saturn",
+        image: "/planets/saturn.png",
+        description: "Famous for its beautiful rings.",
+    },
+    {
+        name: "Uranus",
+        image: "/planets/uranus.png",
+        description: "Rotates on its side.",
+    },
+    {
+        name: "Neptune",
+        image: "/planets/neptune.png",
+        description: "Fastest winds in the solar system.",
+    },
+    {
+        name: "Pluto",
+        image: "/planets/pluto.png",
+        description: "Dwarf planet beyond Neptune.",
+    },
+];
